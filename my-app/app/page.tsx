913ace93b3fb72c8de0657afbccf8ef3a6ab5d1e
AKAGEGE87 <akagege87@gmail.com>
2026-01-27 23:18:00 +0400

    Components created

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,12 @@
+import Latest from "./components/molecules/Latest/Latest";
+import Studio from "./components/molecules/Studio/Studio";
 
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center bg-white font-sans">
       <main className="flex p-20 h-full w-full flex items-center justify-between bg-white">
-
+<Latest/>
+<Studio/>
       </main>
     </div>
   );
