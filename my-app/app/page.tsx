acb2814d2cc7115fba522d64f6654296f2817b3f
Luka Meskhi <meskhiluk@gmail.com>
2026-01-27 21:51:44 +0400

    Create

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,10 +3,7 @@ export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center bg-white font-sans">
       <main className="flex p-20 h-full w-full flex items-center justify-between bg-white">
-          <div className='w-56 h-56 bg-gray-600' ></div>
-          <div className='w-56 h-56 bg-gray-600' ></div>
-          <div className='w-56 h-56 bg-gray-600' ></div>
-          <div className='w-56 h-56 bg-gray-600' ></div>
+          
       </main>
     </div>
   );
