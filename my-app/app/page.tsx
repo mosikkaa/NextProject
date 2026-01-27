b06bb6e493eaafbbd7ea184371790c3e581996d7
Luka Meskhi <meskhiluk@gmail.com>
2026-01-27 22:09:36 +0400

    Atomic Structure

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center bg-white font-sans">
       <main className="flex p-20 h-full w-full flex items-center justify-between bg-white">
-          
+
       </main>
     </div>
   );
