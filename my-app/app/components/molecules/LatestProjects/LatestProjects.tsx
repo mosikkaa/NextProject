d056b55b379cb53ba36470084060ae2c03b94819
tsiklaurii <tsiklaurinino321@gmail.com>
2026-01-28 01:39:52 +0400

    latest-projects/feature
    added latest projects first part

---
+++ my-app/app/components/molecules/LatestProjects/LatestProjects.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Solutions from '../../atoms/Solutions/Solutions'
+
+const LatestProjects = () => {
+    return (
+        <div>
+            <Solutions />
+        </div>
+    )
+}
+
+export default LatestProjects
