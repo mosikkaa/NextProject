8fe4ff3e90a63f4808a6d027428fb08cfd16c157
Luka Meskhi <meskhiluk@gmail.com>
2026-01-29 00:14:34 +0400

    Update Home.tsx

--- my-app/app/components/organisms/Home/Home.tsx
+++ my-app/app/components/organisms/Home/Home.tsx
@@ -1,10 +1,12 @@
 import Land from "@/app/components/molecules/Land/Land";
+import LatestProjects from "@/app/components/molecules/LatestProjects/LatestProjects";
 
 
 const Home = () => {
     return (
         <div>
             <Land/>
+            <LatestProjects></LatestProjects>
         </div>
     )
 }
