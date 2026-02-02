8cb65fa4e44927584ad10b2a283f8aff600f7af1
tsiklaurii <tsiklaurinino321@gmail.com>
2026-02-02 21:36:40 +0400

    fix/NR-2
    added fixed responsive

--- my-app/app/components/atoms/Button/Button.tsx
+++ my-app/app/components/atoms/Button/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
 
 const Button = ({ buttonText, onClick }: ButtonProps) => {
     return (
-        <div className='w-103.5 lg:w-277.5'>
+        <div>
             <button onClick={onClick} className='border border-white rounded-[80px] p-[16px_20px] text-[#FFFFFF] cursor-pointer hover:bg-gray-900 duration-300 ease-in-out tracking-widest'>{buttonText}</button>
         </div>
     )
